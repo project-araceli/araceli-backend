e7fa05a659b770dc5d8ff3a99107ec2c191985f2
xalivo <87896066+xalivo@users.noreply.github.com>
2024-04-02 13:45:30 +0200

    initial commit

---
+++ src/main/java/at/araceli/backend/AraceliBackendApplication.java
@@ -0,0 +1,13 @@
+package at.araceli.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AraceliBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AraceliBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/araceli/backend/AraceliBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.araceli.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AraceliBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
