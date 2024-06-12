30d56868b77c6fa7d62afe90708600fe0622c48a
Michantter <michae.hue.anton@gmail.com>
2024-06-12 10:09:08 +0200

    Fixed Error in User Repo

--- src/main/java/at/araceli/backend/db/UserRepository.java
+++ src/main/java/at/araceli/backend/db/UserRepository.java
@@ -2,6 +2,7 @@ package at.araceli.backend.db;
 
 import at.araceli.backend.pojos.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,5 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
-    Optional<String> getUserPassword(String email);
+    @Query("SELECT u.passwordHash FROM User u WHERE u.email = :email")
+    Optional<String> getUserPasswordHash(String email);
 }
